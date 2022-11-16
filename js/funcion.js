eb05cc72c5de945c7203dd23a12b10fb85460077
Jose Aguilar <jadocenteu@gmail.com>
2022-11-15 20:03:24 -0500

    iniciando JavaScript

---
+++ js/funcion.js
@@ -0,0 +1,18 @@
+const personas = [
+    {
+        nombre: 'Jose', 
+        edad: 33
+    },
+    {
+        nombre: 'Laura', 
+        edad: 28
+    },
+    {
+        nombre: 'Juan', 
+        edad: 41
+    }
+];
+
+export {
+    personas
+}
---
+++ js/index.js
@@ -0,0 +1,52 @@
+/*
+import {personas} from './funcion.js';
+
+console.log(personas);
+
+
+
+
+let numero1=null;
+let numero2=10;
+
+const persona = {nombre: 'Jose', edad: 33};
+console.log("Mi primer JS");
+console.log(persona);
+console.log(persona.edad);
+
+let primos = [ 2, 3, 5, 7, 11, 13];
+const materias = ['Apps', 'Ing Web 1'];
+
+console.log(primos);
+console.log(materias);
+
+console.log(`El tamaño del array de primos es: ${primos.length}`);
+
+for(let i=0;i<materias.length;i++){
+    console.log(`La materia en la posición ${i} es: ${materias[i]}`);
+}
+
+const personas = [
+    {nombre: 'Jose', edad: 33},
+    {nombre: 'Laura', edad: 28},
+    {nombre: 'Juan', edad: 41}
+];
+
+for(let j=0;j<personas.length;j++){
+    console.log(`La persona en la posición ${j} es: ${personas[j].nombre}`);
+}
+
+function getPersona(){
+    return {nombre: 'Laura', edad: 28};
+}
+
+console.log(getPersona());
+
+const getPersona2 = () =>({nombre: 'Laura', edad: 38});
+*/
+const persona2 = {nombre: 'Jose', edad: 33};
+
+const {nombre,edad} = persona2;
+
+console.log(nombre);
+console.log(edad);
